09d045e72b48f6b79cf407c08f2b03715e0dc90b
Aviral Bhardwaj <50884868+aviral-bhardwaj@users.noreply.github.com>
2024-07-07 11:12:07 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,87 +1,48 @@
 from airflow import DAG
-
 from airflow.operators.python_operator import PythonOperator
-
 from airflow.utils.dates import days_ago
-
 from datetime import datetime
-
 import requests
 
-
-
 def print_welcome():
-
     print('Welcome to Airflow!')
 
-
-
 def print_date():
-
     print('Today is {}'.format(datetime.today().date()))
 
-
-
 def print_random_quote():
-
     response = requests.get('https://api.quotable.io/random')
-
     quote = response.json()['content']
-
     print('Quote of the day: "{}"'.format(quote))
 
-
+default_args = {
+    'start_date': days_ago(1),
+}
 
 dag = DAG(
-
     'welcome_dag',
-
-    default_args={'start_date': days_ago(1)},
-
+    default_args=default_args,
     schedule_interval='0 23 * * *',
-
-    catchup=False
-
+    catchup=False,
 )
-
-
 
 print_welcome_task = PythonOperator(
-
     task_id='print_welcome',
-
     python_callable=print_welcome,
-
-    dag=dag
-
+    dag=dag,
 )
-
-
 
 print_date_task = PythonOperator(
-
     task_id='print_date',
-
     python_callable=print_date,
-
-    dag=dag
-
+    dag=dag,
 )
 
-
-
-print_random_quote = PythonOperator(
-
+print_random_quote_task = PythonOperator(
     task_id='print_random_quote',
-
     python_callable=print_random_quote,
-
-    dag=dag
-
+    dag=dag,
 )
-
-
 
 # Set the dependencies between the tasks
-
-print_welcome_task >> print_date_task >> print_random_quote
+print_welcome_task >> print_date_task >> print_random_quote_task
